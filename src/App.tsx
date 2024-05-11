149fbd7ed36659e634de3b84f8ff475d2923051f
Donghee Hong <dh.hong@skku.edu>
2024-05-11 14:00:00 +0900

    [ADD] csv file reader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import Content from "./components/layout/Content";
+import Header from "./components/layout/Header";
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
 function App() {
-  return <div>asfd</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // CSV 파일의 URL 지정
+    const csvFileUrl = "./src/data/ParaSuit_t.csv";
+
+    async function fetchData() {
+      const response = await fetch(csvFileUrl);
+      console.log(response);
+      const reader = response.body?.getReader(); // Add null check for response.body
+      const result = await reader?.read(); // Add null check for reader
+      const decoder = new TextDecoder("utf-8");
+      const csv = decoder.decode(result?.value); // Add null check for result
+
+      // 파싱된 데이터를 상태로 설정
+      Papa.parse(csv, {
+        header: true,
+        dynamicTyping: true,
+        complete: (results) => {
+          setData(results.data);
+        },
+      });
+    }
+
+    fetchData();
+  }, []);
+  console.log(data);
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Content.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div>
+      <h1>Content</h1>
+    </div>
+  );
+};
+export default Content;
